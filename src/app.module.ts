a2180b4ae8e517868ee2dab2b96bf2b58074e070
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-26 16:22:33 +0100

    improve: registered auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       useUnifiedTopology: true,
       useNewUrlParser: true,
     }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
