12d8ddcef1d40a5d3aad030d8545a511b6a0f816
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-26 13:39:36 +0100

    new: db connection

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(`${process.env.DB_URL}`, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
