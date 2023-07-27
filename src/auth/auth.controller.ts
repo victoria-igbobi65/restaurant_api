270a1374218358cddbabd3cfbe9097e774797474
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-27 17:52:13 +0100

    new: auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, HttpException } from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { createUserDto } from './dto/create-user.dto';
+import { loginDto } from './dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: createUserDto) {
+    return this.authService.register(dto);
+  }
+
+  @Post('signin')
+  signin(@Body() dto: loginDto) {
+    if (!dto.email && !dto.phonenumber)
+      throw new HttpException('email or phonenumber is required!', 400);
+    return this.authService.login(dto);
+  }
+}
