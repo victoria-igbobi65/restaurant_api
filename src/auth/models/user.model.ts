404544bf2811aef7a42d3d5df0f9d6cc97f4c044
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-27 17:54:03 +0100

    new: added hooks and methods to user document

--- src/auth/models/user.model.ts
+++ src/auth/models/user.model.ts
@@ -1,4 +1,6 @@
 import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
 
 import { Gender, Role } from 'src/common/constants/enums';
 
@@ -28,6 +30,9 @@ export class User extends Document {
   @Prop({ default: null })
   profilePic: string;
 
+  @Prop({ default: null })
+  address: string;
+
   @Prop({ default: Role.USER })
   role: string;
 
@@ -36,8 +41,58 @@ export class User extends Document {
 
   @Prop({ default: true })
   active: boolean;
+
+  @Prop({ required: true, select: false })
+  password: string;
+
+  @Prop({ default: null })
+  passwordChangedAt: Date;
+
+  @Prop({ default: null })
+  passwordResetToken: string;
+
+  @Prop({ default: null })
+  passwordResetexpires: Date;
+
+  async validatePassword(password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+  }
 }
 export const UserSchema = SchemaFactory.createForClass(User);
+
+/* first pre hook */
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // this checks if password has been not been changed since the last save, to prevent hashing the password again. especially for cases where users just want to update their profile without updating their password
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+/* second pre hook*/
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password') || this.isNew) return next();
+
+  this.passwordChangedAt = new Date(Date.now() - 1000);
+});
+
+UserSchema.methods.validatePassword = async function (
+  password: string,
+): Promise<boolean> {
+  return await bcrypt.compare(password, this.password);
+};
+
+// // This method is used in token validation to check if a user's password has been changed since they were last issued a token.
+// changePasswordAfter(JWTTimestamp: number) {
+//   if (this.passwordChangedAt) {
+//     const changedTimestamp = parseInt(
+//       (+this.passwordChangedAt.getTime() / 1000).toString(),
+//       10,
+//     );
+//     return JWTTimestamp < changedTimestamp;
+//   }
+
+//   return false;
+// }
 
 /* Indexing */
 UserSchema.index({ email: 1, phonenumber: 1 });
