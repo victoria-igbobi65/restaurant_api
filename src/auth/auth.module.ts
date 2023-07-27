290c544a108bfa860c400884c42d4efcec4f49df
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-27 17:52:45 +0100

    new: registered modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { User, UserSchema } from './models/user.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        secret: process.env.SECRET_KEY,
+        signOptions: { expiresIn: process.env.EXPIRES_IN },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
