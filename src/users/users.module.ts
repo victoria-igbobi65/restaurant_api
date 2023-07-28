335a199f7d10cddf40038112f7b4c3f0075a40a0
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-28 19:50:38 +0100

    fix: registered users controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,7 +6,7 @@ import { UsersService } from './users.service';
 import { User } from './models/user.model';
 import { UserSchema } from './models/user.model';
 import { CloudinaryModule } from 'src/common/cloudinary/cloudinary.module';
-//import { UsersController } from './users.controller';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [
@@ -14,7 +14,7 @@ import { CloudinaryModule } from 'src/common/cloudinary/cloudinary.module';
     CloudinaryModule,
     ConfigModule.forRoot({ envFilePath: '.env' }),
   ],
-  controllers: [],
+  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
