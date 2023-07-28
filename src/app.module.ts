aa77ac072cf140dfb91224230d82960b810bb749
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-28 17:34:04 +0100

    improve: registered users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
       useNewUrlParser: true,
     }),
     AuthModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
