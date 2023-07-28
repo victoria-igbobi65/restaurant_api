82b00ff4c41f06a412b1df4fc213fea506eded94
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-28 19:48:47 +0100

    improve: added constants

--- src/common/constants/constants.text.ts
+++ src/common/constants/constants.text.ts
@@ -1,2 +1,5 @@
 export const E_USER_EXISTS = 'User with this credentials exists!';
 export const E_INCORRECT_CREDENTIALS = 'Email or password incorrect';
+export const E_USER_NO_LONGER_EXISTS = 'This User no longer exists!';
+export const E_USER_RECENTLY_CHANGED_PASSWORD =
+  'User recently changed password! please log in again!';
