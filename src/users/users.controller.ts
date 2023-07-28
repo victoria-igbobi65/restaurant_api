03823b604fd1a06177303d6bce73112f801fc05c
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-28 19:49:55 +0100

    feat: added profile pic upload controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,18 +3,26 @@ import {
   Post,
   UseInterceptors,
   UploadedFile,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { AuthGuard } from '@nestjs/passport';
 
 import { UsersService } from './users.service';
+import { CurrentUser } from 'src/auth/decorator/current-user.decorator';
+import { User } from './models/user.model';
 
 @Controller('user')
-export class AuthController {
+@UseGuards(AuthGuard('jwt'))
+export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
-  uploadProfilePic(@UploadedFile() file: Express.Multer.File) {
-    return file;
+  uploadProfilePic(
+    @UploadedFile() file: Express.Multer.File,
+    @CurrentUser() user: User,
+  ) {
+    return this.usersService.uploadProfilePic(file, user.email);
   }
 }
