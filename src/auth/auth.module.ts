32e381dfcd9e837feae89e37c8d3730cbb843483
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-28 17:35:15 +0100

    improve: reflected file changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from 'src/users/users.module';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { User, UserSchema } from './models/user.model';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    UsersModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async () => ({
