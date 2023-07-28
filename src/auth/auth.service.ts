a2a0649278b1c7ec1a718abff1e798dc2e01a569
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-28 17:35:46 +0100

    improve: reflected file change

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,43 +1,35 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
 
-import { User } from './models/user.model';
-import { createUserDto } from './dto/create-user.dto';
-import {
-  E_USER_EXISTS,
-  E_INCORRECT_CREDENTIALS,
-} from 'src/common/constants/constants.text';
+import { User } from 'src/users/models/user.model';
+import { createUserDto } from 'src/users/dto/create-user.dto';
+import { E_INCORRECT_CREDENTIALS } from 'src/common/constants/constants.text';
 import { loginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectModel(User.name) private authModel: Model<User>,
+    private usersService: UsersService,
     private jwtService: JwtService,
   ) {}
 
   async register(dto: createUserDto) {
-    let user = await this.findUserByCredentials(dto.email, dto.phonenumber); // check if user with credentials exists in the db
-    if (user) throw new HttpException(E_USER_EXISTS, 409); // throw error if user with any of the credentials exists
+    const user = await this.usersService.createUser(dto);
+    const token = await this.signToken(user);
 
-    user = await this.authModel.create(dto); // create new user
-    const token = await this.signToken(user); // sign token for new user
-
-    user.password = undefined;
     return { user, token };
   }
 
   async login(dto: loginDto) {
-    const user = await this.findUserByCredentials(dto.email, dto.phonenumber);
-
+    const user = await this.usersService.findUserByCredentials(
+      dto.email,
+      dto.phonenumber,
+    );
     if (!user || !(await user?.validatePassword(dto.password))) {
       throw new HttpException(E_INCORRECT_CREDENTIALS, 400);
     }
-
     const token = await this.signToken(user);
-    user.password = undefined;
     return { user, token };
   }
 
@@ -47,19 +39,5 @@ export class AuthService {
       email: dto.email,
       role: dto.role,
     });
-  }
-
-  async findUserByEmail(email: string) {
-    return this.authModel.findOne({ email: email });
-  }
-
-  async findUserByGoogleId(googleId: string) {
-    return this.authModel.findOne({ googleId: googleId });
-  }
-
-  async findUserByCredentials(email: string, phonenumber: string) {
-    return this.authModel
-      .findOne({ $or: [{ email }, { phonenumber }] })
-      .select('+password');
   }
 }
