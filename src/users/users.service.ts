b6fbb198e5a17bad1e273b5ebccd2103bac0f1c3
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-31 11:16:03 +0100

    improve: update user object and save to db

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,6 +6,7 @@ import { User } from './models/user.model';
 import { createUserDto } from './dto/create-user.dto';
 import { E_USER_EXISTS } from 'src/common/constants/constants.text';
 import { CloudinaryService } from 'src/common/cloudinary/cloudinary.service';
+import { GoogleOauthDto } from 'src/auth/dto/google-oauth.dto';
 
 @Injectable()
 export class UsersService {
@@ -21,10 +22,13 @@ export class UsersService {
     return await this.userModel.create(dto); // create new user
   }
 
-  async uploadProfilePic(file: Express.Multer.File, email: string) {
+  async GoogleOauthCreateUser(dto: GoogleOauthDto) {
+    return await this.userModel.create(dto);
+  }
+
+  async uploadProfilePic(file: Express.Multer.File, user: User) {
     const profilePic = await this.cloudinaryService.uploadImage(file);
 
-    const user = await this.findUserByEmail(email);
     user.profilePic = profilePic.url; // Update user's document with the profile picture url
     await user.save(); // save the updated document to the db
 
@@ -37,6 +41,10 @@ export class UsersService {
 
   async findUserByGoogleId(googleId: string) {
     return this.userModel.findOne({ googleId: googleId });
+  }
+
+  async findUserById(id: string) {
+    return this.userModel.findOne({ _id: id });
   }
 
   async findUserByCredentials(email: string, phonenumber: string) {
