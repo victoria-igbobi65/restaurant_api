db0ef6e4e875018500f3f5effaf354a5bffdcad3
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-31 15:10:06 +0100

    improve: updated file

--- src/common/constants/constants.text.ts
+++ src/common/constants/constants.text.ts
@@ -1,5 +1,6 @@
 export const E_USER_EXISTS = 'User with this credentials exists!';
 export const E_INCORRECT_CREDENTIALS = 'Email or password incorrect';
 export const E_USER_NO_LONGER_EXISTS = 'This User no longer exists!';
+export const E_USER_NOT_EXISTS = 'user not found';
 export const E_USER_RECENTLY_CHANGED_PASSWORD =
   'User recently changed password! please log in again!';
