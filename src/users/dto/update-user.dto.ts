c23a5783d65fb9bed105ed358360889929d30930
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-07-31 15:12:08 +0100

    new: update user dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,43 @@
+import {
+  IsDefined,
+  IsMobilePhone,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
+
 import { createUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(createUserDto) {}
+export class UpdateUserDto extends PartialType(createUserDto) {
+  @IsString()
+  @IsDefined()
+  @IsOptional()
+  @IsNotEmpty({ message: 'username must not be empty!' })
+  readonly username: string;
+
+  @IsString()
+  @IsDefined()
+  @IsOptional()
+  @IsNotEmpty({ message: 'fullname must not be empty!' })
+  readonly fullName: string;
+
+  @IsString()
+  @IsDefined()
+  @IsOptional()
+  @IsNotEmpty({ message: 'address must not be empty!' })
+  readonly address?: string;
+
+  @IsString()
+  @IsDefined()
+  @IsOptional()
+  @IsNotEmpty({ message: 'address must not be empty!' })
+  readonly countryCode?: string;
+
+  @IsString()
+  @IsDefined()
+  @IsOptional()
+  @IsNotEmpty({ message: 'address must not be empty!' })
+  @IsMobilePhone(null, {}, { message: 'Please provide a valid phonenumber!' })
+  readonly phonenumber: string;
+}
