1045d6c99b8034488f487e91fc3645dc9df8b795
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-02 15:27:49 +0100

    feat: implemented forgot and reset password functionalities

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,14 +4,20 @@ import {
   Get,
   Post,
   HttpException,
+  Query,
   Req,
+  Res,
   UseGuards,
+  Patch,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Response, Request } from 'express';
 
 import { AuthService } from './auth.service';
 import { createUserDto } from 'src/users/dto/create-user.dto';
 import { loginDto } from './dto/login.dto';
+import { ForgotPasswordDto } from './dto/forgot-password.dto';
+import { ResetPasswordDto } from './dto/reset-password.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -39,5 +45,24 @@ export class AuthController {
   @UseGuards(AuthGuard('google'))
   googleAuthCallback(@Req() req: any) {
     return this.authService.googleOauthSignIn(req.user);
+  }
+
+  @Post('forgotPassword')
+  async forgotPassword(
+    @Body() dto: ForgotPasswordDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    const baseURL = `${req.protocol}://${req.get('host')}`;
+    await this.authService.forgotPassword(dto.email, baseURL);
+    res.status(200).json({ message: 'Token sent to mail!' });
+  }
+
+  @Patch('resetPassword')
+  async resetPassword(
+    @Body() dto: ResetPasswordDto,
+    @Query('token') token: string,
+  ) {
+    return await this.authService.resetpassword(token, dto.password);
   }
 }
