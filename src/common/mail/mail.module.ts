176b883c871723d03d2cf09bc6ecbfa1768a48e9
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-02 12:37:00 +0100

    feat: email module

---
+++ src/common/mail/mail.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { ConfigService } from '@nestjs/config';
+import * as path from 'path';
+
+import { MailService } from './mail.service';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get('MAIL_HOST'),
+          secure: false,
+          auth: {
+            user: config.get('GMAIL_USER'),
+            pass: config.get('GMAIL_PASS'),
+          },
+        },
+        defaults: {
+          from: `"No Reply" <${config.get('GMAIL_FROM')}>`,
+        },
+        template: {
+          dir: path.resolve(__dirname, 'templates'),
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
