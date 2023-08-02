8f8c3beb527ca1ef6b4be8be0300a0ab4e089b1c
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-02 15:31:16 +0100

    improve: updated file

--- src/common/constants/email-config.text.ts
+++ src/common/constants/email-config.text.ts
@@ -1,2 +1,5 @@
 export const SIGNUP_EMAIL_SUBJECT = 'Welcome to EverythingMeal';
 export const SIGNUP_EMAIL_TEMPLATE = 'welcome';
+
+export const PASSWORD_RESET_SUBJECT = 'Password Reset Token (valid for 10min)';
+export const PASSWORD_RESET_TEMPLATE = 'password-reset';
