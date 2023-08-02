5dac24c5af915283748bbd4c501af96477962b2c
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-02 15:30:37 +0100

    improve: updated file

--- src/common/constants/constants.text.ts
+++ src/common/constants/constants.text.ts
@@ -4,3 +4,4 @@ export const E_USER_NO_LONGER_EXISTS = 'This User no longer exists!';
 export const E_USER_NOT_EXISTS = 'user not found';
 export const E_USER_RECENTLY_CHANGED_PASSWORD =
   'User recently changed password! please log in again!';
+export const E_INVALID_TOKEN = 'Token is invalid or expired!';
