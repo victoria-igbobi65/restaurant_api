675d100565f0ebff23eaaece619ca2b1edf967e6
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-03 22:47:15 +0100

    improve: registered category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    CategoryModule,
   ],
   controllers: [],
   providers: [],
