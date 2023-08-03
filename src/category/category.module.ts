36c54197e9b17d2621211ba6bf63c77cff7ce294
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-03 22:48:12 +0100

    feat: category controller

---
+++ src/category/category.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { CategoryService } from './category.service';
+import { CategoryController } from './category.controller';
+import { Category, CategorySchema } from './models/meal-categories.model';
+import { CloudinaryModule } from 'src/common/cloudinary/cloudinary.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Category.name, schema: CategorySchema },
+    ]),
+    CloudinaryModule,
+  ],
+  controllers: [CategoryController],
+  providers: [CategoryService],
+})
+export class CategoryModule {}
