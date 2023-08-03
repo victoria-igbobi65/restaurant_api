6e3389a7b79fd7b16e38eb99666303df4943bf70
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-03 22:50:24 +0100

    improve: updated file

--- src/common/constants/constants.text.ts
+++ src/common/constants/constants.text.ts
@@ -5,3 +5,6 @@ export const E_USER_NOT_EXISTS = 'user not found';
 export const E_USER_RECENTLY_CHANGED_PASSWORD =
   'User recently changed password! please log in again!';
 export const E_INVALID_TOKEN = 'Token is invalid or expired!';
+export const E_CATEGORY_EXISTS = 'This category already exists!';
+export const E_CATEGORY_NOT_EXISTS = 'ugh ugh!, this category does not exist!';
+export const E_IMAGE_NOT_PROVIDED = 'Image is not provided!';
