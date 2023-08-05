25b24f0fbda06abb64c22c110d3b68c9c47701d7
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-05 07:44:32 +0100

    improve: special tags enum

--- src/common/constants/enums.ts
+++ src/common/constants/enums.ts
@@ -8,3 +8,14 @@ export enum Role {
   USER = 'user',
   ADMIN = 'admin',
 }
+
+export enum specialTags {
+  CHEF_SPECIAL = `chef's special`,
+  VEGAN = 'vegan',
+  VEGETARIAN = 'vegetarian',
+  GLUTEN_FREE = 'gluten free',
+  SPICY = 'spicy',
+  ORGANIC = 'organic',
+  LOW_CALORIE = 'low calorie',
+  HEALTHY = 'healthy',
+}
