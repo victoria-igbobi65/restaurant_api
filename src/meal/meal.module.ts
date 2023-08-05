43bf87fa01a334ceb4fa57e6aa00180b568ec545
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-05 07:46:13 +0100

    new: meal module

---
+++ src/meal/meal.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MealService } from './meal.service';
+import { MealController } from './meal.controller';
+import { CloudinaryModule } from 'src/common/cloudinary/cloudinary.module';
+import { Meal, MealSchema } from './models/meal.model';
+import { CategoryModule } from 'src/category/category.module';
+
+@Module({
+  imports: [
+    CloudinaryModule,
+    CategoryModule,
+    MongooseModule.forFeature([{ name: Meal.name, schema: MealSchema }]),
+  ],
+  controllers: [MealController],
+  providers: [MealService],
+})
+export class MealModule {}
