65cdc92c7ea5c7ed9b6a2c9d6dbd840eb5a0fbdd
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-07 16:44:56 +0100

    new: update meal dto

--- src/meal/dto/update-meal.dto.ts
+++ src/meal/dto/update-meal.dto.ts
@@ -1,4 +1,39 @@
 import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsDefined,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
 import { CreateMealDto } from './create-meal.dto';
+import { specialTags } from 'src/common/constants/enums';
 
-export class UpdateMealDto extends PartialType(CreateMealDto) {}
+export class UpdateMealDto extends PartialType(CreateMealDto) {
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  readonly price?: number;
+
+  @IsString()
+  @IsDefined()
+  @IsNotEmpty()
+  readonly description?: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  readonly preparationTime?: number;
+
+  @IsString({ each: true })
+  @IsDefined()
+  @IsEnum(specialTags, { each: true })
+  @IsNotEmpty({ message: 'label cannot be empty!', each: true })
+  readonly label?: [specialTags];
+
+  @IsString()
+  @IsDefined()
+  @IsNotEmpty()
+  readonly nutritionalInfo?: string;
+}
