f59d45f737ce1bae8efc46f93c843403fc117c92
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-08-07 16:42:33 +0100

    feat: route restriction

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -8,18 +8,25 @@ import {
   UseInterceptors,
   UploadedFile,
   HttpException,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { E_IMAGE_NOT_PROVIDED } from 'src/common/constants/constants.text';
+import { Roles } from 'src/auth/decorator/user-roles.decorator';
+import { Role } from 'src/common/constants/enums';
+import { JwtAuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @UseInterceptors(FileInterceptor('image'))
   create(
     @Body() createCategoryDto: CreateCategoryDto,
@@ -30,16 +37,20 @@ export class CategoryController {
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.categoryService.findAll();
   }
 
   @Get(':slug')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('slug') slug: string) {
     return this.categoryService.findCategoryBySlug(slug);
   }
 
   @Patch('update-image/:slug')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @UseInterceptors(FileInterceptor('image'))
   updateCategoryImage(
     @UploadedFile() file: Express.Multer.File,
